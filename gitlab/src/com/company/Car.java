5e7ae0b9c9886bfddae73b73f4cf39f46af0ec68
GitUser <gituser@git.com>
2015-11-25 10:55:38 +0100

    Hacked by lab

---
+++ gitlab/src/com/company/Car.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Car {
+
+    private String color;
+
+    public Car(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
